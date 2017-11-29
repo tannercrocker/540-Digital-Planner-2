74063de57f45ad4c81e114cb3c89167c524864d7
Benjamin Albrecht <balbrecht1814@gmail.com>
2017-11-29 12:50:14 -0500

    Improved some code readability.

--- 540-Digital-Planner-2/AutoSort/Planner.cs
+++ 540-Digital-Planner-2/AutoSort/Planner.cs
@@ -19,56 +19,48 @@ namespace Digital_Planner.Sorting
 
         public static void GenerateSchedule(int userID)
         {
+            //Debug
             System.Diagnostics.Debug.Print("Generate Schedule");
+
             List<PlannerEvent> autoEvents = new List<PlannerEvent>();
             List<PlannerAvailability> availabilities = new List<PlannerAvailability>();
 
             GetDataFromDatabase(autoEvents, availabilities, userID);
             SortEvents(autoEvents, availabilities);
             AssignWorkDays(autoEvents, availabilities);
-            DebugPrint(autoEvents, availabilities);
+
             db.SaveChanges();
         }
 
-        private static void DebugPrint(List<PlannerEvent> autoEvents, List<PlannerAvailability> days)
+
+        private static void GetDataFromDatabase(List<PlannerEvent> autoEvents, List<PlannerAvailability> availabilities, int userID)
         {
-            System.Diagnostics.Debug.Print("Print Schedule");
-
-            System.Diagnostics.Debug.Print("");
-            System.Diagnostics.Debug.Print("Days List: " + days.Count);
-            System.Diagnostics.Debug.Print("Auto List: " + autoEvents.Count);
-
-            System.Diagnostics.Debug.Print("");
-        }
-
-             
-        private static void GetDataFromDatabase(List<PlannerEvent> autoEvents, List<PlannerAvailability> days, int userID)
-        {
-            //  Gets the information from the database and populates the lists
-
+            //Debug
             System.Diagnostics.Debug.Print("Get Data From Database");
 
-            //Get database records
+            //Get all database records
             List<Event> plannerEvents = db.Events.ToList();
             List<Availability> plannerAvailabilities = db.Availabilities.ToList();
 
-            //sort events by auto/manual assign
+            //get the specified users automatic events
             for (int i = 0; i < plannerEvents.Count; i++)
-            {
                 if (plannerEvents[i].AutoAssign && plannerEvents[i].DPUserID == userID)
                     autoEvents.Add(new PlannerEvent(plannerEvents[i]));
-            }
 
-            System.Diagnostics.Debug.Print("PlannerDays Count: " + plannerAvailabilities.Count);
+            //get the specified user's availabilities
             for (int i = 0; i < plannerAvailabilities.Count; i++)
-                days.Add(new PlannerAvailability(plannerAvailabilities[i]));
+                if (plannerAvailabilities[i].DPUserID == userID)
+                    availabilities.Add(new PlannerAvailability(plannerAvailabilities[i]));
+
+            //Debug
+            System.Diagnostics.Debug.Print("Availabilities List Count: " + availabilities.Count);
+            System.Diagnostics.Debug.Print("Auto List Count: " + autoEvents.Count);
         }
 
 
         private static void SortEvents(List<PlannerEvent> autoEvents, List<PlannerAvailability> days)
         {
-            //  Sorts automatic events based on event score
-
+            //Debug
             System.Diagnostics.Debug.Print("Sort Events");
 
             List<PlannerEvent> temp = new List<PlannerEvent>();
