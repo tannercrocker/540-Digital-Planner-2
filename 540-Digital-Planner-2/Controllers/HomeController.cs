786d0a19eecd8141c4fdb98b47733a4700319ffa
Tanner Crocker <techset7set@gmail.com>
2017-12-03 16:18:01 -0500

    Grammar fixes.

--- 540-Digital-Planner-2/Controllers/HomeController.cs
+++ 540-Digital-Planner-2/Controllers/HomeController.cs
@@ -22,5 +22,9 @@ namespace Digital_Planner.Controllers
             return View();
         }
         
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
